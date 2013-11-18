8dd288fc44a5775230a16003a02dcbf140101f68
Stephanie May <mizmay@gmail.com>
2013-11-17 16:47:24 -0800

    Comment code

--- Plots.R
+++ Plots.R
@@ -1,12 +1,21 @@
 require(ggplot2)
+require(plyr)
 
-Telegraph66th <- read.csv("~/Desktop/bikecountdata/oakland/Telegraph66th.csv")
+# Read in one year of bike count data from Telegraph & 66th in Oakland, 
+# 7 Aug 2012 - 7 Aug 2013 
+Telegraph66th <- read.csv("~/Desktop/bikecountdata/bikecountdata/oakland/Telegraph66th.csv")
+
+# Rename third column
 names(Telegraph66th)[3] <- "count"
+
+# Boxplot of all bike counts by hour
 ggplot(Telegraph66th,(aes(x=X,y=count))) + geom_boxplot() + 
   scale_y_continuous(limits = c(0, 85)) + theme_minimal()
 
+# Hourly averages by month
 Telegraph66th$mo <- sapply(as.character(Telegraph66th$Date), function(x) strsplit(x,'/')[[1]][2])
 Telegraph66th$mo <- factor(Telegraph66th$mo,levels=unique(Telegraph66th$mo))
+# Summarize hourly averages by month, exclude Aug 2013 so that it won't be averaged with Aug 2012
 mnthly <- ddply(Telegraph66th[1:8616,],.(X,mo),summarise,mean_count = mean(count,na.rm = T))
 ggplot(mnthly,aes(y=mean_count,x=X,color=mo)) + geom_vline(xintercept = 17,color="grey") + 
   geom_point() + 
@@ -15,11 +24,11 @@ ggplot(mnthly,aes(y=mean_count,x=X,color=mo)) + geom_vline(xintercept = 17,color
   scale_y_discrete("Bike Count") + ggtitle("Telegraph and 66th, Oakland, CA") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+# Plot daily counts for the whole period
 Telegraph66th$y <- as.numeric(Telegraph66th$X) - 1
 dates <- unique(Telegraph66th$Date)
 Telegraph66th$Date <- factor(Telegraph66th$Date,levels=as.character(dates))
 Telegraph66th$x <- as.numeric(Telegraph66th$Date)
-
 ylabels = unique(Telegraph66th$X)
 xlabels = as.character(unique(Telegraph66th$Date))
 mondays <- seq(1, length(xlabels), 7) - 1
