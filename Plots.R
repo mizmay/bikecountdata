8cc99bf5fa94b701855ac5d881c865a5e57c3047
Stephanie May <mizmay@gmail.com>
2013-11-18 18:06:22 -0800

    Fix axes on hourly averages by month

--- Plots.R
+++ Plots.R
@@ -13,6 +13,9 @@ ggplot(Telegraph66th,(aes(x=X,y=count))) + geom_boxplot() +
   scale_y_continuous(limits = c(0, 85)) + theme_minimal()
 
 # Hourly averages by month
+hours <- as.character(unique(Telegraph66th$X))
+hours[-seq(1,length(hours),4)] <- ""
+# extract the month from the date
 Telegraph66th$mo <- sapply(as.character(Telegraph66th$Date), function(x) strsplit(x,'/')[[1]][2])
 Telegraph66th$mo <- factor(Telegraph66th$mo,levels=unique(Telegraph66th$mo))
 # Summarize hourly averages by month, exclude Aug 2013 so that it won't be averaged with Aug 2012
@@ -20,8 +23,8 @@ mnthly <- ddply(Telegraph66th[1:8616,],.(X,mo),summarise,mean_count = mean(count
 ggplot(mnthly,aes(y=mean_count,x=X,color=mo)) + geom_vline(xintercept = 17,color="grey") + 
   geom_point() + 
   facet_wrap(~mo) + theme_minimal() + scale_color_discrete("Month") +
-  scale_x_discrete("Time of Day") +
-  scale_y_discrete("Bike Count") + ggtitle("Telegraph and 66th, Oakland, CA") +
+  scale_x_discrete("Time of Day",labels=hours) +
+  scale_y_continuous("Bike Count") + ggtitle("Telegraph and 66th, Oakland, CA") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 # Plot daily counts for the whole period
@@ -32,7 +35,7 @@ Telegraph66th$x <- as.numeric(Telegraph66th$Date)
 ylabels = unique(Telegraph66th$X)
 xlabels = as.character(unique(Telegraph66th$Date))
 mondays <- seq(1, length(xlabels), 7) - 1
-xlabels[-mondays] <- ""
+xlabels[-mondays] <- "" # Only label dates on Mondays
 ggplot(subset(Telegraph66th,count < 100),(aes(x=x,y=y))) + 
   geom_vline(xintercept = mondays,color='gray') + 
   geom_tile(alpha=0.6,aes(fill=count)) + 
